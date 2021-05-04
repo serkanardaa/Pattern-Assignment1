07e68483e3ad2c9e5d869836503fb57c514b4e8f
Jonas Cederberg <jonasced@kth.se>
2021-05-04 18:01:16 +0200

    Fixed all bugs in Forward

--- PattRecClasses/MarkovChain.py
+++ PattRecClasses/MarkovChain.py
@@ -125,30 +125,37 @@ class MarkovChain:
         a_temp = np.empty([self.nStates,T])  # temporary forward variable
         ahat = np.empty([self.nStates,T])  # scaled forward varialbes 
         if self.is_finite:
-            c = np.zeros([1,T+1])
+            c = np.zeros(T+1)
         else:
-            c = np.zeros([1,T])  # forward scale factors
+            c = np.zeros(T)  # forward scale factors
 
         ### Initialization: First element (index 0) in each list
         # a_temp[:,0] calculation
         for j in range(self.nStates):
             a_temp[j,0] = self.q[j]*pX[0,j]  # b in original is substituted by pX 
         # c[0] calculation
-        c[0] = np.sum(a_temp[:,0])
+        for index in range(a_temp[:,0].shape[0]):  # ugly solution to bug below
+            c[0] = c[0] + a_temp[index,0] 
+        # c[0] = np.sum(a_temp[:,0])  # WHY DOES THIS NOT WORK?
+        #print(a_temp.shape)
+        #print(ahat[:,0].shape, a_temp[:,0].shape)
+
         # ahat[:,0] calculation
         ahat[:,0] = a_temp[:,0] / c[0]
 
         ### Forward step:
         for t in range(1,T): 
             for j in range(self.nStates):
-                a_temp[j,t] = pX[t,j]*np.multiply(ahat[:,t-1],self.A[:,j])  # multiplying the two row vectors ahat[:,t-1] and A[:,j] sums them directly
+                output = pX[t,j]*np.matmul(ahat[:,t-1],self.A[:,j])  # multiplying the two row vectors ahat[:,t-1] and A[:,j] sums them directly
+                a_temp[j,t] = output
             c[t] = np.sum(a_temp[:,t])
             ahat[:,t] = a_temp[:,t] / c[t]
         
         ### Termination:
         if self.is_finite:
-            c[T] = np.multiply(ahat[:,T],self.A[:,self.nStates])  # same here
+            c[T] = np.matmul(ahat[:,T-1],self.A[:,self.nStates])  # same here
         return [ahat, c]
+
 
     def viterbi(self):
         pass
