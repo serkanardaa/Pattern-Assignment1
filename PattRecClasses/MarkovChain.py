a9db82d94643ed9e7b24bc499d6be8d8e85f79c0
Jonas Cederberg <jonas.cederberg@gmail.com>
2021-05-07 09:33:16 +0200

    Bugfixes

--- PattRecClasses/MarkovChain.py
+++ PattRecClasses/MarkovChain.py
@@ -119,7 +119,7 @@ class MarkovChain:
         Compatible with both finite and infinite HMM.
         """
         
-        T = pX.shape1[1]  # extracts the length of the observed sequence
+        T = pX.shape[1]  # extracts the length of the observed sequence
 
         # Variable allocation
         a_temp = np.empty([self.nStates,T])  # temporary forward variable
